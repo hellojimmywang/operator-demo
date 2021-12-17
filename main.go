c0a558c761d4b451b9aee641ca42ed9776a73c4c
hellojimmywang <582698081@qq.com>
2021-12-17 22:01:52 +0800

    second

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Mysqlcluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.MysqlbackupReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Mysqlbackup")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
